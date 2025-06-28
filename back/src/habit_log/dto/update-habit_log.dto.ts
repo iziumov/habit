16b99beacea28df36d6a17fddf6be8fb93fc443c
iziumov <ihoriziumov@gmail.com>
2025-06-29 00:55:18 +0300

    Refactor code structure for improved readability and maintainability

--- back/src/habit_log/dto/update-habit_log.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateHabitLogDto } from './create-habit_log.dto';
-
-export class UpdateHabitLogDto extends PartialType(CreateHabitLogDto) {}
--- back/src/habit_log/habit_log.module.ts
+++ back/src/habit_log/habit_log.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HabitLogService } from './habit_log.service';
 import { HabitLogController } from './habit_log.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [JwtModule],
   controllers: [HabitLogController],
   providers: [HabitLogService],
 })
